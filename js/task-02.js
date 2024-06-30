71e1730db3a6ba5d6be3fd5976cb4497c3907552
Mihaela <26812755+ellamm@users.noreply.github.com>
2024-06-30 10:39:02 +0300

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulIngredients = document.querySelector('#ingredients');
+const arrayOfItems = ingredients.map(ingredient => {
+  const item = document.createElement('li');
+  item.textContent = ingredient;
+  item.classList.add('item');
+  return item
+});
+ulIngredients.append(...arrayOfItems);
+console.log(ingredients);
